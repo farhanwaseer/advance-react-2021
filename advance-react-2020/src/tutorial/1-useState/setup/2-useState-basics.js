f8ac2d7bc86e72624a348ec1b971bed5727dd101
farhanwaseer <farhanaliwaseer@gmail.com>
2021-06-10 00:17:22 +0500

    .

--- advance-react-2020/src/tutorial/1-useState/setup/2-useState-basics.js
+++ advance-react-2020/src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,18 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-
-   const [text, setText] = useState("Random text");
-   const handler = () => {
-     setText("Hello world")
-   }
+  const [text, setText] = useState("Random text");
+  const handler = () => {
+    setText("Hello world");
+  };
   return (
-<React.Fragment>
-<h2> {text}</h2>
- <button className="btn" onClick={handler}>change Text</button>
- 
-  </React.Fragment>
-  )
+    <React.Fragment>
+      <h2> {text}</h2>
+      <button className="btn" onClick={handler}>
+        change Text
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
