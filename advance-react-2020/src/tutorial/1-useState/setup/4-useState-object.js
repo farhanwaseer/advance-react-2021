ebb5d5d7f401d2a80ac46b4248f77e94fbaeb924
farhanwaseer <farhanaliwaseer@gmail.com>
2021-06-11 00:43:37 +0500

    .

--- advance-react-2020/src/tutorial/1-useState/setup/4-useState-object.js
+++ advance-react-2020/src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
+const [person, setPerson] = useState({name: "farhan", age: 21, message: "random message"});
+console.log(person)
+
   return <h2>useState object example</h2>;
 };
 
