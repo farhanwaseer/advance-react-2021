5332e35a586c4989653733dd2dbca2b078f41304
farhanwaseer <farhanaliwaseer@gmail.com>
2021-06-11 01:25:36 +0500

    .

--- advance-react-2020/src/tutorial/1-useState/setup/5-useState-counter.js
+++ advance-react-2020/src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [count, setCount] = useState(0);
+  const reset = () => {
+    setCount(0);
+  };
+
+  return (
+    <>
+      <section style={{ margin: "4rem 0" }}>
+        <h2>useState counter example</h2>
+        <h1>{count}</h1>
+        <button className="btn" onClick={() => setCount(count + 1)}>
+          Increase
+        </button>
+        <button className="btn" onClick={reset}>
+          reset
+        </button>
+        <button className="btn" onClick={() => setCount(count - 1)}>
+          Decrease
+        </button>
+      </section>
+    </>
+  );
 };
 
 export default UseStateCounter;
