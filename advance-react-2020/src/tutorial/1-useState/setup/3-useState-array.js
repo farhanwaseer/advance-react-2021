016338e6c8d6dbe95fdb3028d74d6f21c66dc329
farhanwaseer <farhanaliwaseer@gmail.com>
2021-06-10 18:52:31 +0500

    .

--- advance-react-2020/src/tutorial/1-useState/setup/3-useState-array.js
+++ advance-react-2020/src/tutorial/1-useState/setup/3-useState-array.js
@@ -5,6 +5,11 @@ const UseStateArray = () => {
 
    const [people, setPeople] = React.useState(data);
 
+   const removeItem = (id) => {
+     let newPeople = people.filter((person) => person.id !== id);
+     setPeople(newPeople);
+   }
+
   return <>
    {
      people.map((person) => {
@@ -14,6 +19,7 @@ const UseStateArray = () => {
       return (
         <div key={id} className="item">
           <h3>{name}</h3>
+          <button onClick={()=> removeItem(id)}>remove</button>
         </div>
       );
      })
