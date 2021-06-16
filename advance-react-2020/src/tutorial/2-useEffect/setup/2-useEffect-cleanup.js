bb5596e90eb8284adc6329d2c88ead2ca9324f22
farhanwaseer <farhanaliwaseer@gmail.com>
2021-06-16 23:16:33 +0500

    .

--- advance-react-2020/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ advance-react-2020/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -6,6 +6,10 @@ import React, { useState, useEffect } from 'react';
 const UseEffectCleanup = () => {
    const [size, setSize] = useState(window.innerWidth);
    console.log(size);
+ 
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  }
 
    useEffect(()=> {
      window.addEventListener('resize', checkSize);
