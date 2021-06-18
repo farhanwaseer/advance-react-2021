6a08b139ed86df763270e5c7af8e215329b398d4
farhanwaseer <farhanaliwaseer@gmail.com>
2021-06-18 23:54:08 +0500

    .

--- advance-react-2020/src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ advance-react-2020/src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -1,19 +1,20 @@
 import React, { useState, useEffect } from "react";
 
 const url = "https://api.github.com/users";
-
 const UseEffectFetchData = () => {
   const [users, setUsers] = useState([]);
 
   const getUsers = async () => {
     const response = await fetch(url);
-    const users = await response.json;
+    const users = await response.json();
+    setUsers(users);
     console.log(users);
   };
 
+
   useEffect(() => {
     getUsers();
-  });
+  }, []);
 
   return (
     <>
