26e40eb477d584fefbbdebc648788484af46b32d
farhanwaseer <farhanaliwaseer@gmail.com>
2021-06-20 17:16:52 +0500

    .

--- advance-react-2020/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ advance-react-2020/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-const url = "https://api.github.com/users/QuincyLarson";
+const url = "https://api.github.com/users/QuincyLars,on";
 const MultipleReturns = () => {
   const [loading, setLoading] = useState(true);
   const [isError, setIsError] = useState(false);
@@ -8,7 +8,15 @@ const MultipleReturns = () => {
   useEffect(() => {
     
     fetch(url)
-      .then((resp) => resp.json())
+      .then((resp) => {
+        if(resp.status >= 200 && resp.status <= 299) {
+          return resp.json();
+        }else{
+          setLoading(false);
+          setIsError(true);
+          throw new Error (resp.statusText);
+        }
+      })
       .then((user) => {
         const {login} = user;
         setUser(login);
