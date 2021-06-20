f56f5b353bdce25dfcd2940bea5183ac663d6120
farhanwaseer <farhanaliwaseer@gmail.com>
2021-06-20 16:59:08 +0500

    .

--- advance-react-2020/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ advance-react-2020/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -7,7 +7,8 @@ const MultipleReturns = () => {
 
   useEffect(() => {
     fetch(url)
-      .then((resp) => resp.json)
+      .then((resp) => resp.json())
+      .then((user) => console.log(user))
       .catch((error) => console.log(error));
   }, []);
 
