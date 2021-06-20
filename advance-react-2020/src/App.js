c6a861539d5d0f20d10e1f302a2344999ed04a8a
farhanwaseer <farhanaliwaseer@gmail.com>
2021-06-20 16:24:16 +0500

    .

--- advance-react-2020/src/App.js
+++ advance-react-2020/src/App.js
@@ -6,7 +6,7 @@ import Setup from "./tutorial/3-conditional-rendering/setup/1-multiple-returns";
 function App() {
   return (
     <div className='container'>
-      <h2>Advanced Tutorial</h2>
+      {/* <h2>Advanced Tutorial</h2> */}
       <Setup />
     </div>
   )
