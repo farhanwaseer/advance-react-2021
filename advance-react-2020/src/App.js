583bfac6fd29083bd2f1b258e69d557654153175
farhanwaseer <farhanaliwaseer@gmail.com>
2021-06-20 17:25:03 +0500

    .

--- advance-react-2020/src/App.js
+++ advance-react-2020/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Setup from "./tutorial/3-conditional-rendering/setup/1-multiple-returns";
+import Setup from "./tutorial/3-conditional-rendering/setup/2-short-circuit";
 
 
 function App() {
