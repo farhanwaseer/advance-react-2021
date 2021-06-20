a780722d5eb9d6c209df001cd18f053554c73505
farhanwaseer <farhanaliwaseer@gmail.com>
2021-06-20 16:49:48 +0500

    .

--- advance-react-2020/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ advance-react-2020/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,26 +1,35 @@
-import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import React, { useState, useEffect } from "react";
+const url = "https://api.github.com/users/QuincyLarson";
 const MultipleReturns = () => {
- const [loading,setLoading] = useState(false);
- const [isError, setIsError] = useState(false);
- const [user, setUser] = useState("default user");
+  const [loading, setLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState("default user");
 
- useEffect(() => {
-   fetch(url).then((resp) => resp.json).catch(error => console.log(error))
- }, [])
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => resp.json)
+      .catch((error) => console.log(error));
+  }, []);
 
-  if(loading){
-    return <div><h2>Loading...</h2></div>;
-
+  if (loading) {
+    return (
+      <div>
+        <h2>Loading...</h2>
+      </div>
+    );
   }
-  if(isError){
-    return <div>
-      <h2>Error...</h2>
-    </div>;
+  if (isError) {
+    return (
+      <div>
+        <h2>Error...</h2>
+      </div>
+    );
   }
-  return <div>
-    <h1>{user}</h1>
-  </div>;
+  return (
+    <div>
+      <h1>{user}</h1>
+    </div>
+  );
 };
 
 export default MultipleReturns;
