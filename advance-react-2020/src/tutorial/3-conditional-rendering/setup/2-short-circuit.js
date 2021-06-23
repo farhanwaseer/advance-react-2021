8bef954049245871b98204c7ec602b7afa638a09
farhanwaseer <farhanaliwaseer@gmail.com>
2021-06-23 23:38:03 +0500

    .

--- advance-react-2020/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ advance-react-2020/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,15 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  const [text, setText] = useState('');
-  const firstValue = text || 'hello world';
-  const secondValue = text && 'hello world';
+  const [text, setText] = useState("");
+  const firstValue = text || "hello world";
+  const secondValue = text && "hello world";
 
   return (
-    <h1>{text || 'Farhan'}</h1>
-  )
+    <>
+      <h1>{text || "Farhan"}</h1>
+      <button className="btn">toggle error</button>
+    </>
+  );
 };
 
 export default ShortCircuit;
