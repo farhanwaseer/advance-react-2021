dc6fb271848755b00b577aceba066e642e68e721
farhanwaseer <farhanaliwaseer@gmail.com>
2021-06-26 00:32:15 +0500

    .

--- advance-react-2020/src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ advance-react-2020/src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -14,6 +14,10 @@ const ControlledInputs = () => {
       <label htmlFor="firstName">Name :</label>
       <input type="text" name="firstName" id="firstName" />
     </div>
+    <div className='form-control'>
+      <label htmlFor="email">Email :</label>
+      <input type="text" name="email" id="email" />
+    </div>
   </form>
 </article>
     </>
