7d2c7991ca983fd55a252f71c867231a2ad52ea8
farhanwaseer <farhanaliwaseer@gmail.com>
2021-06-26 01:59:17 +0500

    .

--- advance-react-2020/src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ advance-react-2020/src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -12,17 +12,17 @@ const ControlledInputs = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(firstName && email) {
-      const person = {firstName, email};
-      setPeople((people) =>{
+    if (firstName && email) {
+      const person = { firstName, email };
+      setPeople((people) => {
         return [...people, person];
       });
-      setFirstName('');
-      setEmail('');
-      console.log(person)
-    } else{
-      console.log('empty values');
-    }   
+      setFirstName("");
+      setEmail("");
+      console.log(person);
+    } else {
+      console.log("empty values");
+    }
   };
   return (
     <>
@@ -36,18 +36,31 @@ const ControlledInputs = () => {
               name="firstName"
               id="firstName"
               value={firstName}
-              onChange={(e)=> setFirstName(e.target.value)}
+              onChange={(e) => setFirstName(e.target.value)}
             />
           </div>
           <div className="form-control">
             <label htmlFor="email">Email :</label>
-            <input type="text" name="email" id="email" value={email}
-              onChange={(e)=> setEmail(e.target.value)} />
+            <input
+              type="text"
+              name="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <button type="submit">add person</button>
         </form>
       </article>
-     
+      {people.map((person) => {
+        const { id, firstName, email } = person;
+        return (
+          <div className="item" key={id}>
+            <h4>{firstName}</h4>
+            <p>{email}</p>
+          </div>
+        );
+      })}
     </>
   );
 };
