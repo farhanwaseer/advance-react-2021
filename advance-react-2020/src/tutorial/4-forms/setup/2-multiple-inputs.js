93a19039756c6beced46d59d8d6b53218d17f998
farhanwaseer <farhanaliwaseer@gmail.com>
2021-06-27 01:05:17 +0500

    .

--- advance-react-2020/src/tutorial/4-forms/setup/2-multiple-inputs.js
+++ advance-react-2020/src/tutorial/4-forms/setup/2-multiple-inputs.js
@@ -9,6 +9,7 @@ import React, { useState } from 'react';
 const ControlledInputs = () => {
   const [firstName, setFirstName] = useState('');
   const [email, setEmail] = useState('');
+  const [age, setAge] = useState('');
   const [people, setPeople] = useState([]);
 
   const handleSubmit = (e) => {
