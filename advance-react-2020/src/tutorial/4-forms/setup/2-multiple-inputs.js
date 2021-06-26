1ee659fc674b1287fca1a8cbcf32c64a638534ca
farhanwaseer <farhanaliwaseer@gmail.com>
2021-06-27 02:27:53 +0500

    .

--- advance-react-2020/src/tutorial/4-forms/setup/2-multiple-inputs.js
+++ advance-react-2020/src/tutorial/4-forms/setup/2-multiple-inputs.js
@@ -17,11 +17,17 @@ const ControlledInputs = () => {
   const handleChange = (e) => {
    const name = e.target.name;
    const value = e.target.value;
+   setPerson({...person, [name]: value});
    console.log(name, value);
   }
  
   const handleSubmit = (e) => {
    e.preventDefault();
+   if(person.firstName && person.email && person.age) {
+     const newPerson = {...person, id: new Date().getTime().toString()};
+     setPeople([...people, newPerson]);
+     setPerson({firstName:'', email:'', age:''})
+   }
   }
   return (
     <>
@@ -60,10 +66,11 @@ const ControlledInputs = () => {
           <button type='submit' onClick={handleSubmit}>add person</button>
         </form>
         {people.map((person, index) => {
-          const { id, firstName, email } = person;
+          const { id, firstName, email, age } = person;
           return (
             <div className='item' key={id}>
               <h4>{firstName}</h4>
+              <p>{age}</p>
               <p>{email}</p>
             </div>
           );
