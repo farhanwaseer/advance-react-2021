74c847d824a042f487e64998d49b2e004ff4a054
farhanwaseer <farhanaliwaseer@gmail.com>
2021-06-28 01:13:16 +0500

    .

--- advance-react-2020/src/tutorial/6-useReducer/setup/index.js
+++ advance-react-2020/src/tutorial/6-useReducer/setup/index.js
@@ -1,9 +1,12 @@
-import React, { useState, useReducer } from 'react';
-import Modal from './Modal';
-import { data } from '../../../data';
+import React, { useState, useReducer } from "react";
+import Modal from "./Modal";
+import { data } from "../../../data";
 // reducer function
 
 const Index = () => {
+  const [people, setPeople] = useState(data);
+  const [showModal, setShowModal] = useState(false);
+
   return <h2>useReducer</h2>;
 };
 
