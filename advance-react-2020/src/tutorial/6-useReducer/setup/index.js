533c70b6648f3ece8a48ad508057294d8be8e26b
farhanwaseer <farhanaliwaseer@gmail.com>
2021-06-28 01:21:14 +0500

    .

--- advance-react-2020/src/tutorial/6-useReducer/setup/index.js
+++ advance-react-2020/src/tutorial/6-useReducer/setup/index.js
@@ -4,12 +4,22 @@ import { data } from "../../../data";
 // reducer function
 
 const Index = () => {
+  const [name, setName] = useState('');
   const [people, setPeople] = useState(data);
   const [showModal, setShowModal] = useState(false);
 
+const handleSubmit = (e) => {
+e.preventDefault();
+}
+
   return <>
-  useReducer
+  <h2>useReducer</h2>
   {showModal && <Modal />}
+  <form onSubmit={handleSubmit}>
+    <div>
+      <input type="text"  value={name} onChange={()=> setName(e.target.value)}/>
+    </div>
+  </form>
   </>;
 };
 
