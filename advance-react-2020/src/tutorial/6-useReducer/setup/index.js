11052f4795ab23bc1e0d28f1f63fedc15609a17c
farhanwaseer <farhanaliwaseer@gmail.com>
2021-06-28 23:44:26 +0500

    .

--- advance-react-2020/src/tutorial/6-useReducer/setup/index.js
+++ advance-react-2020/src/tutorial/6-useReducer/setup/index.js
@@ -3,13 +3,22 @@ import Modal from "./Modal";
 import { data } from "../../../data";
 // reducer function
 
+const reducer = (state, action) => {
+
+}
+
 const Index = () => {
   const [name, setName] = useState("");
-  const [people, setPeople] = useState(data);
-  const [showModal, setShowModal] = useState(false);
-
+  const [state, dispatch] = useReducer(reducer);
   const handleSubmit = (e) => {
     e.preventDefault();
+    if(name){
+      setShowModal(true);
+      setPeople([...people, {id: new Date().getTime().toString(),name}]);
+      setName('');
+    }else{
+      setShowModal(true);
+    }
   };
 
   return (
@@ -26,6 +35,12 @@ const Index = () => {
         </div>
         <button type='submit'>add</button>
       </form>
+    {people.map((person)=> {
+      return <div key={person.id}>
+        <h4>{person.name} <p>{person.id}</p></h4>
+        
+      </div>
+    })}
     </>
   );
 };
