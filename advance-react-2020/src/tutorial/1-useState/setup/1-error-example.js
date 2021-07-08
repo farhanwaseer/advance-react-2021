7fb575ccbbbf30597eca1752bbc3533f30aa0a15
farhanwaseer <farhanaliwaseer@gmail.com>
2021-07-08 23:02:57 +0500

    .

--- advance-react-2020/src/tutorial/1-useState/setup/1-error-example.js
+++ advance-react-2020/src/tutorial/1-useState/setup/1-error-example.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
 const ErrorExample = () => {
-  let title = 'title random';
+  var title = 'title random';
 
   const handleClick = () => {
     title = 'hello people';
   }
 
   return (
-    <>
+    <React.Fragment>
     <h2>{title}</h2>
     <button type='button' className='btn' onClick={handleClick}>change button</button>
-    </>
+    </React.Fragment>
   );
 };
 
