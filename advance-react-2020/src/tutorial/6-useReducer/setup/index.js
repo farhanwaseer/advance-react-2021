0d2fea509643591feedc3b4d173df60e9efc1a0b
farhanwaseer <farhanaliwaseer@gmail.com>
2021-08-10 20:13:58 +0500

    .

--- advance-react-2020/src/tutorial/6-useReducer/setup/index.js
+++ advance-react-2020/src/tutorial/6-useReducer/setup/index.js
@@ -1,22 +1,55 @@
 import React, { useState, useReducer } from "react";
 import Modal from "./Modal";
 import { data } from "../../../data";
+
 // reducer function
 
+const reducer = (state, action) => {
+  console.log(state,action)
+   return state;
+};
+
+const defaultState = {
+  people: [],
+  isModalOpen: false,
+  modalContent: "",
+};
+
 const Index = () => {
-  const [name,setName] = useState('');
-  const [people,setPeople] = useState(data);
-  const [showModal,setShowModal] = useState(false);
-  
+  const [name, setName] = useState("");
+  const [state, dispatch] = useReducer(reducer, defaultState);
+ 
+ 
+  const handleSubmit = (e) => {
+    e.perventDefault();
+    if (name) {
+      dispatch({ type: "TESTING" });
+    } else {
+      dispatch({type: 'RANDOM'})
+    }
+  };
+
   return (
     <>
-   {showModal && <Modal/>}
-   <form onSubmit={handleSubmit}>
-     <div>
-       <input type="text" value={name} onChange={(e)=> setName(e.target.value)} />
-     </div>
-   </form>
-   </>
+      {state.isModalOpen && <Modal modalContent={state.modalContent} />}
+      <form className="form" onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <button type="submit">add</button>
+      </form>
+      {state.people.map((person) => {
+        return (
+          <div key={person.id}>
+            <h4>{person.name}</h4>
+          </div>
+        );
+      })}
+    </>
   );
 };
 
