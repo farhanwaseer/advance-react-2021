66121a95204d93be30056638452f9ab0a3841c6a
farhanwaseer <farhanaliwaseer@gmail.com>
2021-08-26 21:16:56 +0500

    .

--- advance-react-2020/src/tutorial/10-prop-types/setup/Product.js
+++ advance-react-2020/src/tutorial/10-prop-types/setup/Product.js
@@ -1,12 +1,14 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import  defaultImage from '../../../assets/default-image.jpeg';
 
 const Product = ({ name, image, price }) => {
   return (
     <article className="product">
-   <h4>Single product</h4>
-      {/* <h4>{name}</h4>
-      <p>${price}</p> */}
+   {/* <h4>Single product</h4> */}
+          <img src={image.url} alt={name} />
+      <h4>{name}</h4>
+      <p>${price}</p> 
     </article>
   );
 };
@@ -15,6 +17,12 @@ Product.propTypes = {
  image:PropTypes.object.isRequired,
  name:PropTypes.string.isRequired,
  price:PropTypes.number.isRequired
+}
+
+Product.defaultProps = {
+  name: 'default name',
+  price: 3.99,
+  image:defaultImage
 }
 
 export default Product;
