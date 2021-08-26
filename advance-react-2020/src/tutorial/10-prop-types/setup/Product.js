18d46ff9bfbaba13ea5e761ee57c58fe79239542
farhanwaseer <farhanaliwaseer@gmail.com>
2021-08-26 20:55:52 +0500

    .

--- advance-react-2020/src/tutorial/10-prop-types/setup/Product.js
+++ advance-react-2020/src/tutorial/10-prop-types/setup/Product.js
@@ -3,8 +3,9 @@ import React from "react";
 const Product = ({ name, image, price }) => {
   return (
     <article className="product">
-      <h4>{name}</h4>
-      <p>${price}</p>
+   <h4>Single product</h4>
+      {/* <h4>{name}</h4>
+      <p>${price}</p> */}
     </article>
   );
 };
