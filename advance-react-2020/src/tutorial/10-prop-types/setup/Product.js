cfa5ad220d12970fb03bb11b57cbad0fb11e260a
farhanwaseer <farhanaliwaseer@gmail.com>
2021-08-26 20:48:50 +0500

    .

--- advance-react-2020/src/tutorial/10-prop-types/setup/Product.js
+++ advance-react-2020/src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Product = ({name, image,price}) => {
-  return <article className='product'></article>;
+  return <article className='product'><h4>{name}</h4></article>;
 };
 
 export default Product;
